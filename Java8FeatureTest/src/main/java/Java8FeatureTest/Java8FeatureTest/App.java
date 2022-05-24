41648d7ee11ea1d85d603dea25603ba34620dc83
HP <HP@RajuKumar>
2022-05-24 09:35:01 +0530

    First Commit

---
+++ Java8FeatureTest/src/main/java/Java8FeatureTest/Java8FeatureTest/App.java
@@ -0,0 +1,13 @@
+package Java8FeatureTest.Java8FeatureTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
